737939b73899452c2c7f0685c99461831b3357c5
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-04-05 17:24:46 +0500

    Add save method to Book class

--- book.rb
+++ book.rb
@@ -16,4 +16,17 @@ class Book < Item
   def can_be_archived?
     super || @cover_state == 'bad'
   end
+  
+  def self.save_all(books)
+    data = books.map do |book|
+      {
+        title: book.title,
+        author: book.author,
+        genre: book.genre,
+        publish_date: book.publish_date,
+        cover_state: book.cover_state
+      }
+    end
+    File.write('./data/books.json', JSON.pretty_generate(data))
+  end
 end
