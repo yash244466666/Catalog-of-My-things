c1033d8a2b571e0bad50594c337fd45e4bbe2717
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-04-05 10:00:30 +0500

    Fix rubocop lint errors

--- label.rb
+++ label.rb
@@ -2,14 +2,14 @@ require_relative 'item'
 
 class Label
   attr_reader :id, :title, :color, :items
-  
+
   def initialize(id, title, color)
     @id = id
     @title = title
     @color = color
     @items = []
   end
-  
+
   def add_item(item)
     @items << item
     item.label = self
